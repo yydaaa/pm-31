18c0e8d056f56693a1b398004b55f354685394b7
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-10-02 13:50:32 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan,namaBelakang,email) => {
+  console.log("Nama : " + namaDepan+" "+ namaBelakang)
+  console.log("email :"+ email)
+  console.log("Login Screen")
+}
+
+login("Yuda","Apriana Salam","222310285@smkn4-tsm.sch.id")
---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan,namaBelakang,email) {
+  console.log("Nama : " + namaDepan +" "+ namaBelakang)
+  console.log("email :"+ email)
+  console.log("Selamat Datang di Home Screen")
+}
+
+home("Yuda","Apriana Salam","222310285@smkn4-tsm.sch.id")
